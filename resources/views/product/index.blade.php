3eb7da03394f3153b6c6f166ce2680151c835673
NerdyKimmy <jurijvenz@gmail.com>
2025-02-24 00:03:28 +0200

    Product cards fixed size

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -4,7 +4,7 @@
     <x-input type="text" name="search" placeholder="Search for the products" value="{{request()->get('search')}}"/>
     </form>
     <div
-        class="grid gap-8 grig-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-5"
+        class="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-stretch p-5"
     >
         @foreach($products as $product)
             <!-- Product Item -->
@@ -15,16 +15,21 @@
                     'title' => $product->title,
                     'price' => $product->price,
                 ]) }})"
-                class="border border-1 border-gray-200 rounded-md hover:border-pink-300 transition-colors bg-white"
+                class=" flex flex-col h-full border border-1 border-gray-200 rounded-md hover:border-pink-300 transition-colors bg-white"
             >
-                <a href="{{ route('product.view', $product->slug) }}" class="aspect-w-3 aspect-h-2 block overflow-hidden ">
+                <a
+                    href="{{ route('product.view', $product->slug) }}"
+                    class="block overflow-hidden h-80"
+                >
                     <img
                         src="{{ $product->image }}"
-                        alt=""
-                        class="object-cover rounded-lg hover:scale-105 hover:rotate-1 transition-transform"
+                        alt="{{ $product->title }}"
+                        class="object-cover w-full h-full transition-transform rounded-lg
+               hover:scale-105 hover:rotate-1"
                     />
                 </a>
-                <div class="p-4">
+
+                <div class="p-4 flex-1">
                     <h3 class="text-lg">
                         <a href="{{ route('product.view', $product->slug) }}">
                             {{$product->title}}
@@ -32,27 +37,7 @@
                     </h3>
                     <h5 class="font-bold">₴{{$product->price}}</h5>
                 </div>
-                <div class="flex justify-between py-3 px-4">
-                    <button
-                        @click="addToWatchlist()"
-                        class="w-10 h-10 rounded-full border border-1 border-pink-300 flex items-center justify-center hover:bg-pink-300 hover:text-white active:bg-pink-500 transition-colors"
-                        :class="isInWatchlist(id) ? 'bg-pink-300 text-white' : 'text-pink-300'"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                            />
-                        </svg>
-                    </button>
+                <div class="p-4">
                     <button class="btn-primary" @click="addToCart(id)">
                         Add to Cart
                     </button>
