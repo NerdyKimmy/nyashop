9706235986c4d250b86b14a83920ae22383ca7ae
NerdyKimmy <jurijvenz@gmail.com>
2025-02-23 23:42:21 +0200

    Added searching for client side

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
-
+use \Illuminate\Database\Query\Builder;
 class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::query()->orderBy('updated_at', 'desc')->paginate(8);
+        $search=request()->get('search');
+        $products = Product::query()
+            ->where(function ($query) use ($search) {
+                /** @var $query \Illuminate\Database\Query\Builder */
+                $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('description', 'like', '%' . $search . '%');
+
+            })
+            ->orderBy('updated_at', 'desc')
+            ->paginate(8);
         return view('product.index', [
             'products' => $products
         ]);
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,4 +1,8 @@
 <x-app-layout>
+
+    <form action="" method="GET">
+    <x-input type="text" name="search" placeholder="Search for the products" value="{{request()->get('search')}}"/>
+    </form>
     <div
         class="grid gap-8 grig-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-5"
     >
